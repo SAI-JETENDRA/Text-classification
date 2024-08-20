ee148bbd688e0127dd9cce5d499402100df2f794
SAI-JETENDRA <158267751+SAI-JETENDRA@users.noreply.github.com>
2024-08-20 18:44:20 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+'Logistic Regression': joblib.load('Logistic Regression.pkl'),
+    'Random Forest': joblib.load('Random Forest.pkl'),
+    'SVM': joblib.load('SVM.pkl'),
+    'Naive Bayes': joblib.load('Naive Bayes.pkl')
